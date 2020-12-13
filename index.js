ab4ee78245e19a27e2792bd0a3d8245f6a537ba2
Sharon Wachingagit config --global user.name Sharon <wachingawathiri@gmail.com>
2020-12-14 02:13:20 +0300

    ordering module

---
+++ index.js
@@ -0,0 +1,19 @@
+var productId,itemQuantity,itemName,itemPrice,discount;
+var order = itemQuantity * itemPrice;
+
+if(itemQuantity >= 10 && 25)
+{
+    discount = 10;
+}
+else if(itemQuantity > 25 && 50)
+{
+    discount = 25;
+}
+else if(itemQuantity >50)
+{
+    discount = 50;
+}
+else
+{
+    invalid;
+}
